b56f9e19de8c2ff74bda5092acc2ebae8c5faffc
Henrique Marques <MarquesRick@users.noreply.github.com>
2019-08-31 14:54:16 -0300

    Commit with IntelliJ

---
+++ Java-Avancado/src/fundamentos/Area/AreaCircunferencia.java
@@ -0,0 +1,12 @@
+package fundamentos.Area;
+
+public class AreaCircunferencia {
+
+	public static void main(String[] args) {
+		Raio raio = new Raio();
+	
+		raio.calculaArea(10);
+		raio.printarTela();
+		
+	}
+}
