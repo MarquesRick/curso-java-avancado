cd2875c6effd729e63209cb0d849ff04ada9fa2d
marques-rick <hmservicostech@outlook.com.br>
2024-04-19 18:19:43 -0300

    change old code

--- Java-Avancado/src/fundamentos/Temperatura/Celsius.java
+++ Java-Avancado/src/fundamentos/Temperatura/Celsius.java
@@ -4,38 +4,38 @@ public class Celsius {
 	
 	public double celsius;
 	public double fahrenheit;
-	public final double ajuste = 5.0 / 9.0;
-	public final double fator = 32;
+	public final double ADJUSTMENT = 5.0 / 9.0;
+	public final double FACTOR = 32;
 	
 	Celsius(){
 		//contsrutor
 	}
 	
-	public void comentarioTemperatura(double celsius){
+	public void comentarioTemperatura(){
 		
 		if(celsius < 0){
-			System.out.println("Frio Pakas.." + celsius);
+			System.out.println("Too cold.. " + (int)celsius);
 		} else if(celsius > 40){
-			System.out.println("Calor.." +  celsius);
+			System.out.println("Hot day.. " +  (int)celsius);
 		} else{
-			System.out.println(celsius);
+			System.out.println("Mild temperature");
 		}
 	}
 	
 	double calculaCelsius(double fah){
-		return celsius = ((fah - fator) * ajuste);
+		return celsius = ((fah - FACTOR) * ADJUSTMENT);
 	}
 	
-	void printaCel(){
-		System.out.println("A medida convertida ? " + (int)celsius + "?C"); //Cast (double to int)
+	void printCel(){
+		System.out.println("A medida convertida: " + (int)celsius + "C"); //Cast (double to int)
 	}
 	
 	double calculaFah(double cel){
 		return fahrenheit = ((9 * cel + 160) / 5);
 	}
 	
-	void printaFah(){
-		System.out.println("A medida convertida ? " + (int)fahrenheit + "?F\n"); //Cast (double to int)
+	void printFah(){
+		System.out.println("A medida convertida: " + (int)fahrenheit + "F\n"); //Cast (double to int)
 	}
 	
 }
--- Java-Avancado/src/fundamentos/Temperatura/Temperatura.java
+++ Java-Avancado/src/fundamentos/Temperatura/Temperatura.java
@@ -8,9 +8,11 @@ public class Temperatura {
 		System.out.print("\nConversor de temperatura: Graus Celsios -> Fahrenheit\n");
 		System.out.print("-----------------------------------------------------\n");
 		c.calculaCelsius(80);
-		c.printaCel();
+		c.printCel();
 		c.calculaFah(26);
-		c.printaFah();
+		c.printFah();
+
+		c.comentarioTemperatura();
 
 	}
 }
